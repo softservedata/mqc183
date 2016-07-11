8d694b1ec614a8b924d3ad1d531f95f2d5f1ac0b
yaroslav <garasym@gmail.com>
2016-07-11 13:38:35 +0300

    example java pro

---
+++ src/test/java/com/softserve/edu/AppTest.java
@@ -0,0 +1,15 @@
+package com.softserve.edu;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AppTest 
+{
+
+    @Test
+    public void testApp()
+    {
+        Assert.assertTrue( true );
+    }
+    
+}
